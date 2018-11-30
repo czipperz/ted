740f9e8f45cb5d2ff448afe6b6c19e57cb67b0ee
Czipperz <czipperz@gmail.com>
2018-11-30 17:55:21 -0500

    Handle control space

--- ted_core/src/curses_display.rs
+++ ted_core/src/curses_display.rs
@@ -73,6 +73,8 @@ impl Display for CursesDisplay {
 fn convert_to_key(c: char, alt: bool) -> Input {
     if c == '\n' /* 10 */ {
         Input::Key { key: c, control: false, alt }
+    } else if c == 0 as char /* 0 */ {
+        Input::Key { key: '@', control: true, alt }
     } else if c >= 1 as char && c <= 26 as char {
         Input::Key { key: (c as u8 - 1 + 'a' as u8) as char,
                      control: true, alt }
