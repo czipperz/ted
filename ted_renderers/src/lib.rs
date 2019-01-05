e76ceb70f15f8d4a9f4dca759827a7af226eec29
Czipperz <czipperz@gmail.com>
2019-01-04 19:31:13 -0500

    Add gtk component

--- ted_renderers/src/lib.rs
+++ ted_renderers/src/lib.rs
@@ -1,6 +1,11 @@
+extern crate gtk;
+extern crate gdk;
 extern crate pancurses_result;
 extern crate parking_lot;
 extern crate ted_core;
 
 mod curses_renderer;
 pub use curses_renderer::*;
+
+mod gtk_renderer;
+pub use gtk_renderer::*;
